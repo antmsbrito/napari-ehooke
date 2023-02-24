bb675483380a84c299b4258a1510d27c0c8d704b
António Brito <50997716+antmsbrito@users.noreply.github.com>
2023-02-24 09:57:21 +0000

    Turned 2 widgets into one

--- src/napari_ehooke/_computemask.py
+++ src/napari_ehooke/_computemask.py
@@ -1,12 +1,14 @@
 """
 TODO
 """
-from typing import TYPE_CHECKING
 
-from magicgui import magic_factory
+import typing
+from typing import TYPE_CHECKING
 
 if TYPE_CHECKING:
     import napari
+
+from magicgui import magic_factory
 
 import numpy as np
 from scipy import ndimage, signal
@@ -15,21 +17,21 @@ from skimage.morphology import binary_closing, binary_dilation
 from skimage.transform import EuclideanTransform, warp
 
 @magic_factory(algorithm={"choices":["Isodata","Local Average"]})
-def compute_mask(image:"napari.types.ImageData",algorithm="Isodata",blocksize:int=151,offset:float=0.02,closing:int=1,dilation:int=0,fillholes:bool=False)->"napari.types.LabelsData":
+def compute_mask(Base:"napari.types.ImageData",Fluor:"napari.types.ImageData",algorithm="Isodata",blocksize:int=151,offset:float=0.02,closing:int=1,dilation:int=0,fillholes:bool=False,autoalign:bool=False)->typing.List["napari.types.LayerDataTuple"]:
     """
     TODO    
     """
 
     if algorithm == "Isodata":
-        
-        mask = image > threshold_isodata(image)
+
+        mask = Base > threshold_isodata(Base)
         mask = mask.astype(int)
         mask = 1 - mask
 
     elif algorithm == "Local Average":
         if blocksize%2==0:
             blocksize += 1
-        mask = image > threshold_local(image, block_size=blocksize, method="gaussian", offset=offset)
+        mask = Base > threshold_local(Base, block_size=blocksize, method="gaussian", offset=offset)
         mask = mask.astype(int)
         mask = 1 - mask
     
@@ -45,22 +47,17 @@ def compute_mask(image:"napari.types.ImageData",algorithm="Isodata",blocksize:in
     if fillholes:
         mask = ndimage.binary_fill_holes(mask)
 
-    return mask
 
+    if autoalign:
+        corr = signal.fftconvolve(mask,Fluor[::-1,::-1])
+        deviation = np.unravel_index(np.argmax(corr),corr.shape)
+        cm = ndimage.center_of_mass(np.ones(corr.shape))
+        
+        dy,dx = np.subtract(deviation,cm)
+        matrix = EuclideanTransform(rotation=0, translation=(dx,dy))
 
-@magic_factory
-def align_mask(mask:"napari.types.LabelsData", fluor:"napari.types.ImageData")->"napari.types.ImageData":
-    """
-    TODO
-    """
+        aligned_fluor = warp(Fluor, matrix.inverse, preserve_range=True) # TODO check if fluor intensity values stay the same
 
-    corr = signal.fftconvolve(mask,fluor[::-1,::-1])
-    deviation = np.unravel_index(np.argmax(corr),corr.shape)
-    cm = ndimage.center_of_mass(np.ones(corr.shape))
-    
-    dy,dx = np.subtract(deviation,cm)
-    matrix = EuclideanTransform(rotation=0, translation=(dx,dy))
-
-    return warp(fluor, matrix.inverse, preserve_range=True) # TODO check if fluor intensity values stay the same
-
-    
+        return [(mask, {'name': 'Mask'}, 'Labels'), (aligned_fluor,{'name':'Aligned fluor'}, 'Image')]
+    else:
+        return [(mask, {'name': 'Mask'}, 'Labels'),]
