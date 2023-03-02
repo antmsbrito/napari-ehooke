5cb7317c508de4f8a755af81c1f8eede1855fabe
António Brito <50997716+antmsbrito@users.noreply.github.com>
2023-03-02 11:44:32 +0000

    Fixed example data parenthesis

--- src/napari_ehooke/_sample_data.py
+++ src/napari_ehooke/_sample_data.py
@@ -7,10 +7,10 @@ import numpy
 from skimage.io import imread
 
 def phase_example():
-    return [(imread("https://github.com/antmsbrito/napari-ehooke/raw/main/docs/test_phase.tif",{"name":"Example S.aureus phase contrast"}, "image")),]
+    return [(imread("https://github.com/antmsbrito/napari-ehooke/raw/main/docs/test_phase.tif"),{"name":"Example S.aureus phase contrast"}, "image",)]
 
 def membrane_example():
-    return [(imread("https://github.com/antmsbrito/napari-ehooke/raw/main/docs/test_membrane.tif",{"name":"Example S.aureus labeled with membrane dye"}, "image")),]
+    return [(imread("https://github.com/antmsbrito/napari-ehooke/raw/main/docs/test_membrane.tif"),{"name":"Example S.aureus labeled with membrane dye"}, "image",)]
 
 def dna_example():
-    return [(imread("https://github.com/antmsbrito/napari-ehooke/raw/main/docs/test_dna.tif",{"name":"Example S.aureus labeled with DNA dye"}, "image")),]
+    return [(imread("https://github.com/antmsbrito/napari-ehooke/raw/main/docs/test_dna.tif"),{"name":"Example S.aureus labeled with DNA dye"}, "image",)]
