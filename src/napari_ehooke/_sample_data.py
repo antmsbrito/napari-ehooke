1c87f95554d480a434b3b5de8e2876fe45daad61
António Brito <50997716+antmsbrito@users.noreply.github.com>
2023-04-10 12:33:16 +0100

    Style

--- src/napari_ehooke/_sample_data.py
+++ src/napari_ehooke/_sample_data.py
@@ -1,5 +1,5 @@
 """
-TODO
+Module resposible for downloading sample data of S. aureus microscopy images
 """
 from __future__ import annotations
 
@@ -7,10 +7,13 @@ import numpy
 from skimage.io import imread
 
 def phase_example():
-    return [(imread("https://github.com/antmsbrito/napari-ehooke/raw/main/docs/test_phase.tif"),{"name":"Example S.aureus phase contrast"}, "image",)]
+    return [(imread("https://github.com/antmsbrito/napari-ehooke/raw/main/docs/test_phase.tif"),
+             {"name":"Example S.aureus phase contrast"}, "image",)]
 
 def membrane_example():
-    return [(imread("https://github.com/antmsbrito/napari-ehooke/raw/main/docs/test_membrane.tif"),{"name":"Example S.aureus labeled with membrane dye"}, "image",)]
+    return [(imread("https://github.com/antmsbrito/napari-ehooke/raw/main/docs/test_membrane.tif"),
+             {"name":"Example S.aureus labeled with membrane dye"}, "image",)]
 
 def dna_example():
-    return [(imread("https://github.com/antmsbrito/napari-ehooke/raw/main/docs/test_dna.tif"),{"name":"Example S.aureus labeled with DNA dye"}, "image",)]
+    return [(imread("https://github.com/antmsbrito/napari-ehooke/raw/main/docs/test_dna.tif"),
+             {"name":"Example S.aureus labeled with DNA dye"}, "image",)]
