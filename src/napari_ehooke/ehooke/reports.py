fab5a282fdab50ff7dbbbd37d51a70d1b8194908
António Brito <50997716+antmsbrito@users.noreply.github.com>
2024-05-07 16:23:25 +0100

    .csv report and other improvements

--- src/napari_ehooke/ehooke/reports.py
+++ src/napari_ehooke/ehooke/reports.py
@@ -1,7 +1,8 @@
 ﻿"""Module used to create the report of the cell identification"""
+import pandas as pd
 import matplotlib as mpl
 from skimage.io import imsave
-from skimage.util import img_as_float, img_as_uint
+from skimage.util import img_as_float, img_as_uint, img_as_ubyte
 from skimage.filters import threshold_isodata
 from skimage.color import gray2rgb
 from decimal import Decimal
@@ -13,15 +14,15 @@ from .cellprocessing import stats_format
 
 class ReportManager:
 
-    def __init__(self, parameters, cells):
-        self.cells = cells
+    def __init__(self, parameters,properties,allcells):
+        self.cells = allcells
+        self.properties = properties
         self.params = parameters
         self.keys = stats_format(parameters)
 
         self.cell_data_filename = None
 
     def html_report(self, filename):
-        params = self.params
         cells = self.cells
         """generates an html report with the all the cell stats from the
         selected cells"""
@@ -40,113 +41,51 @@ class ReportManager:
         report = [HTML_HEADER]
 
         if len(cells) > 0:
-            header = '<table>\n<th>Cell ID</th><th>Images'
+            header = '<table>\n<th>Cell ID</th><th>Images'      
             for k in self.keys:
                 label, digits = k
                 header = header + '</th><th>' + label
             header += '</th>\n'
             selects = ['\n<h1>Selected cells:</h1>\n' + header + '\n']
-            rejects = ['\n<h1>Rejected cells:</h1>\n' + header + '\n']
-
-            count = 0
-            count2 = 0
 
             print("Total Cells: " + str(len(cells)))
 
             for cell in cells:
-                if cell.selection_state == 1:
-                    cellid = str(int(cell.label))
-                    img = img_as_float(cell.image)
+                
+                cellid = str(int(cell.label))
 
-                    """
-                    x0, y0, x1, y1 = cell.box
-                    memb_img = cell.fluor[x0:x1 + 1, y0:y1 + 1]
-                    dna_img = cell.optional[x0:x1 + 1, y0:y1 + 1]
-                    imwrite(filename + "/_images/membrane" + os.sep + cellid + '.tif', memb_img)
-                    imwrite(filename + "/_images/dna" + os.sep + cellid + '.tif', dna_img)
-                    imsave(filename + "/_images/crops" + os.sep + cellid + '.png', img)
-                    """
-                    
-                    imsave(filename + "/_images" +
-                           os.sep + cellid + '.png', img)
-                    lin = '<tr><td>' + cellid + '</td><td><img src="./' + '_images/' + \
-                          cellid + '.png" alt="pic" width="200"/></td>'
+                img = img_as_ubyte(cell.image)
 
-                    count += 1
+                imsave(filename+"/_images"+os.sep+cellid+'.png',img)
 
-                    for stat in self.keys:
-                        lbl, digits = stat
-                        number = ("{0:." + str(digits) + "f}").format(cell.stats[lbl])
-                        number = str(Decimal(number))
-                        number = number.rstrip("0").rstrip(".") if "." in number else number
-                        lin = lin + '</td><td>' + number
+                lin = '<tr><td>' + cellid + '</td><td><img src="./_images/'+cellid+'.png" alt="pic" width="200"/></td>'
 
-                    lin += '</td></tr>\n'
-                    selects.append(lin)
 
-                elif cell.selection_state == 0:
-                    cellid = str(int(cell.label))
-                    img = img_as_float(cell.image)
-                    imsave(filename + "/_rejected_images" +
-                           os.sep + cellid + '.png', img)
-                    lin = '<tr><td>' + cellid + '</td><td><img src="./' + '_rejected_images/' + \
-                          cellid + '.png" alt="pic" width="200"/></td>'
+                for stat in self.keys:
+                    lbl, digits = stat
+                    number = ("{0:." + str(digits) + "f}").format(cell.stats[lbl])
+                    number = str(Decimal(number))
+                    number = number.rstrip("0").rstrip(".") if "." in number else number
+                    lin = lin + '</td><td>' + number
 
-                    count2 += 1
+                lin += '</td></tr>\n'
+                selects.append(lin)
 
-                    for stat in self.keys:
-                        lbl, digits = stat
-                        number = ("{0:." + str(digits) + "f}").format(cell.stats[lbl])
-                        number = str(Decimal(number))
-                        number = number.rstrip("0").rstrip(".") if "." in number else number
-                        lin = lin + '</td><td>' + number
-
-                    lin += '</td></tr>\n'
-                    rejects.append(lin)
-
-            print("Selected Cells: " + str(count))
-            print("Rejected Cells: " + str(count2))
 
             report.append(
                 "\n<h1>napari-eHooke Report - <a href='TODO' target='_blank'>wiki</a></h1>")
 
-            report.append("\n<h3>Total cells: " + str(count + count2) + "</h3>")
-            report.append("\n<h3>Selected cells: " + str(count) + "</h3>")
-            report.append("\n<h3>Rejected cells: " + str(count2) + "</h3>")
+            report.append("\n<h3>Total cells: " + str(len(self.properties['label'])) + "</h3>")
 
-            if params['classify_cell_cycle']:
-                p1count = 0
-                p2count = 0
-                p3count = 0
+            if self.params['classify_cell_cycle']:
+                _,pcounts=np.unique(self.properties['Cell Cycle Phase'], return_counts=True)
 
-                for cell in cells:
-
-                    if cell.selection_state == 1:
-                        if cell.stats["Cell Cycle Phase"] == 1:
-                            p1count += 1
-                        elif cell.stats["Cell Cycle Phase"] == 2:
-                            p2count += 1
-                        elif cell.stats["Cell Cycle Phase"] == 3:
-                            p3count += 1
-
-                report.append("\n<h3>Phase 1 cells: " + str(p1count) + "</h3>")
-                report.append("\n<h3>Phase 2 cells: " + str(p2count) + "</h3>")
-                report.append("\n<h3>Phase 3 cells: " + str(p3count) + "</h3>")
+                report.append("\n<h3>Phase 1 cells: " + str(pcounts[0]) + "</h3>")
+                report.append("\n<h3>Phase 2 cells: " + str(pcounts[1]) + "</h3>")
+                report.append("\n<h3>Phase 3 cells: " + str(pcounts[2]) + "</h3>")
             
-            params['units'] = 'um' # TODO
-            if params['units'] == "um":
-                report.append(
-                    "\n<h3>Pixel size: " + str(params['pixel_size']) + " x " + str(params['pixel_size']) + " " + "\u03BC" + "m" + "</h3>")
-            else:
-                report.append(
-                    "\n<h3>Pixel size: " + str(params['pixel_size']) + " x " + str(params['pixel_size']) + " " + params['units'] + "</h3>")
-
             if len(selects) > 1:
                 report.extend(selects)
-                report.append('</table>\n')
-
-            if len(rejects) > 1:
-                report.extend(rejects)
                 report.append('</table>\n')
 
             report.append('</body>\n</html>')
@@ -172,11 +111,9 @@ class ReportManager:
 
             if not os.path.exists(filename + "/_images"):
                 os.makedirs(filename + "/_images")
-                os.makedirs(filename + "/_images/membrane")
-                os.makedirs(filename + "/_images/dna")
-                os.makedirs(filename + "/_images/crops")
-            if not os.path.exists(filename + "/_rejected_images"):
-                os.makedirs(filename + "/_rejected_images")
+                #os.makedirs(filename + "/_images/membrane")
+                #os.makedirs(filename + "/_images/dna")
+                #os.makedirs(filename + "/_images/crops")
         else:
             filename = path + "/Report_" + report_id + "_1"
             filename = self.check_filename(filename)
@@ -184,13 +121,16 @@ class ReportManager:
 
             if not os.path.exists(filename + "/_images"):
                 os.makedirs(filename + "/_images")
-                os.makedirs(filename + "/_images/membrane")
-                os.makedirs(filename + "/_images/dna")
-                os.makedirs(filename + "/_images/crops")
-            if not os.path.exists(filename + "/_rejected_images"):
-                os.makedirs(filename + "/_rejected_images")
+                #os.makedirs(filename + "/_images/membrane")
+                #os.makedirs(filename + "/_images/dna")
+                #os.makedirs(filename + "/_images/crops")
+
 
         self.html_report(filename)
+
+        df = pd.DataFrame(self.properties)
+        df.to_csv(os.path.join(filename,f"Analysis.csv"))
+
 
         # TODO add view of selected cells
         # TODO SAVE PARS
